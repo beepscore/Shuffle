5e16c1fd4b35517b56b44a2d158a17547b6286ce
beepscore <support@beepscore.com>
2015-06-12 01:19:08 -0700

    Add README.md and BSShuffler

---
+++ Shuffle/BSShuffler.h
@@ -0,0 +1,28 @@
+//
+//  BSShuffler.h
+//  Shuffle
+//
+//  Created by Steve Baker on 6/12/15.
+//  Copyright (c) 2015 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BSShuffler : NSObject
+
+/** @return YES if shuffledString is a valid "shuffle" of string0 and string1.
+ *  else return NO.
+ *  shuffledString is valid if it can be formed by interleaving
+ *  the characters of string0 and string1 in a way that maintains
+ *  the left to right ordering of the characters from each string.
+ *
+ *  Example:
+ *  YES == [BSShuffler isValidShuffle:@"dabecf"
+ *                           ofString:@"abc"
+ *                         withString:@"def"];
+ */
++(BOOL) isValidShuffle:(NSString *)shuffledString
+              ofString:(NSString *)string0
+            withString:(NSString *)string1;
+
+@end
