9e811c09de410cbcd791e8a198cac4ae1f90cec8
beepscore <support@beepscore.com>
2015-06-16 15:01:32 -0700

    Add BSStringUtils

---
+++ Shuffle/BSStringUtils.h
@@ -0,0 +1,13 @@
+//
+//  BSStringUtils.h
+//  Shuffle
+//
+//  Created by Steve Baker on 6/16/15.
+//  Copyright (c) 2015 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BSStringUtils : NSObject
+
+@end
---
+++ Shuffle/BSStringUtils.m
@@ -0,0 +1,13 @@
+//
+//  BSStringUtils.m
+//  Shuffle
+//
+//  Created by Steve Baker on 6/16/15.
+//  Copyright (c) 2015 Beepscore LLC. All rights reserved.
+//
+
+#import "BSStringUtils.h"
+
+@implementation BSStringUtils
+
+@end
