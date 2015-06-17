ee8c4fb9ee9f17d283063485b1c037adc9b4f30b
beepscore <support@beepscore.com>
2015-06-16 21:50:57 -0700

    In BSShuffler.h revise docstring

--- Shuffle/BSShuffler.h
+++ Shuffle/BSShuffler.h
@@ -11,8 +11,7 @@
 
 @interface BSShuffler : NSObject
 
-/** @return YES if shuffledString is a valid "shuffle" of string0 and string1.
- *  else return NO.
+/**
  *  shuffledString is valid if it can be formed by interleaving
  *  the characters of string0 and string1 in a way that maintains
  *  the left to right ordering of the characters from each string.
@@ -21,6 +20,13 @@
  *  YES == [BSShuffler isValidShuffle:@"dabecf"
  *                           ofString:@"abc"
  *                         withString:@"def"];
+ * Traverses binary tree breadth first.
+ * Uses a queue instead of recursion to reduce risk of call stack overflow.
+ * @param shuffledString a potential shuffle of string0 and string1
+ * @param string0 a source string
+ * @param string1 a source string
+ * @return YES if shuffledString is a valid shuffle of string0 and string1
+ * else return NO
  */
 -(BOOL) isValidShuffle:(NSString *)shuffledString
               ofString:(NSString *)string0
