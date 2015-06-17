0a080aeb110a7b6e56be23eb4185431f62e618d8
beepscore <support@beepscore.com>
2015-06-16 22:02:20 -0700

    In BSNodeTests delete unused setUp and tearDown

--- ShuffleTests/BSNodeTests.m
+++ ShuffleTests/BSNodeTests.m
@@ -15,16 +15,6 @@
 
 @implementation BSNodeTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
 - (void)testNodePropertiesNil {
 
     BSNode *node = [[BSNode alloc] init];
